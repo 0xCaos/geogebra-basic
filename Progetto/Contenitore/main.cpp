6f3c524f207575658f7976ab9a2b305142c5d686
matteocasonato <matteocasonato@protonmail.com>
2020-12-28 11:10:45 +0100

    Test con il main

--- Progetto/Contenitore/main.cpp
+++ Progetto/Contenitore/main.cpp
@@ -1,48 +1,116 @@
 #include <iostream>
 #include "Vettore.h"
 #include <vector>
+using namespace std;
+
+// ERASE --> gestione out of range?
+// CLEAR --> aggiusta la size ma non la capacity
+// CAPACITY --> potenza di 2 più vicina alla size?
+// Operator[i] --> 0 <= i < size()
 
 int main() {
-    std::cout << "Tester contenitore: INIZIO" << std::endl;
-    /*
-    Vettore<int> v1 = {10};
-    Vettore<std::pair<int, int>> vett_coppie;
-
-    vett_coppie.push_back({2,2});
-    vett_coppie.push_back({3,3});
-    vett_coppie.push_back({4,4});
-
-    std::cout << "Prima stampa \n";
-    for(auto el : vett_coppie) {
-        std::cout << "(" << el.first << ", " << el.second << ") ";
-    }
-
-    //vett_coppie.resize(10); // costruttore di default per val
-    vett_coppie.resize(10, {7,7}); // costruttore di copia per val
-
-    std::cout << "\nSeconda stampa \n";
-    for(auto el : vett_coppie) {
-        std::cout << "(" << el.first << ", " << el.second << ") ";
-    }
-
-    std::cout << "\n" << vett_coppie.size() << " " << vett_coppie.capacity();
-     */
-
     Vettore<int> v1 = {10, 20, 30, 40, 50};
-    Vettore<int> v3;
-    std::vector<int> v2 = {77, 77, 77, 40, 50};
 
-    //v1.insert(v1.begin(),77);
+    for(int i=51; i<60; ++i)
+        v1.push_back(i);
 
-    v1.insert(v1.end()-1,{6,7,8,9});
-    //v2.insert(v2.begin()-1, 77);
-    //v1.insert(v1.begin()+2, 66);
+    cout << "----- TEST #1 -----" << endl;
 
+    cout << "----- BEFORE CLEAR -----" << endl;
+    cout << "STAMPA ELEMENTI: ";
+    for(auto el : v1)
+        cout << el << " ";
+    cout << endl;
+    cout << "SIZE: " << v1.size() << endl;
+    cout << "CAPACITY: " << v1.capacity() << endl;
+    cout << "EMPTY?: " << v1.empty() << endl;
 
-    v3 = v1;
+    cout << "BEGIN: " << *(v1.begin()) << endl;
+    cout << "BEGIN == END? " << (v1.end() == v1.begin()) << endl;
+
+    cout << endl;
+
+    v1.clear();
+    cout << "----- AFTER CLEAR -----" << endl;
+    cout << "STAMPA ELEMENTI: ";
+    for(auto el : v1)
+        cout << el << " ";
+    cout << endl;
+    cout << "SIZE: " << v1.size() << endl;
+    cout << "CAPACITY: " << v1.capacity() << endl;
+    cout << "EMPTY?: " << v1.empty() << endl;
+
+    cout << "BEGIN: " << *(v1.begin()) << endl;
+    cout << "BEGIN == END? " << (v1.end() == v1.begin()) << endl;
+    cout << endl;
+
+    cout << "----- TEST #2 -----" << endl;
+    Vettore<string> v2 = {"Midro", "Noghez", "Spola", "PNX", "Mamma"};
+    cout << "STAMPA ELEMENTI: ";
+    for(auto el : v2)
+        cout << el << " ";
+    cout << endl;
+    v2.erase(v2.begin()+2);
+    cout << "ERASE Begin+2: ";
+    for(auto el : v2)
+        cout << el << " ";
+    v2.insert(v2.begin()+2, {"Add1", "Add2", "Add3"});
+    cout << endl;
+    cout << "INSERT initializer list: ";
+    for(auto el : v2)
+        cout << el << " ";
+    v2.insert(v2.begin()+5, 3, "PIOLO");
+    cout << endl;
+    cout << "INSERT Same element: ";
+    for(auto el : v2)
+        cout << el << " ";
+    cout << endl << endl;
+
+    cout << "----- TEST #3 -----" << endl;
+    Vettore<string> v3 = {"Padre", "Figlio", "Spirito"};
+    cout << "STAMPA ELEMENTI: ";
     for(auto el : v3)
-        std::cout << el << " ";
-
-    std::cout << "\n";
-    std::cout << v3.size() << " " << v3.capacity();
+        cout << el << " ";
+    cout << endl;
+    /*
+    cout << "v3=v2: ";
+    v3 = v2;
+    for(auto el : v3)
+        cout << el << " ";
+    */
+    cout << endl;
+    cout << "----- v2 Props -----" << endl;
+    cout << "SIZE: " << v2.size() << endl;
+    cout << "CAPACITY: " << v2.capacity() << endl;
+    cout << "EMPTY?: " << v2.empty() << endl;
+    cout << endl;
+    cout << "----- v3 Props -----" << endl;
+    cout << "SIZE: " << v3.size() << endl;
+    cout << "CAPACITY: " << v3.capacity() << endl;
+    cout << "EMPTY?: " << v3.empty() << endl;
+    cout << endl;
+    cout << "v2[100]: " << v2[100] << "... (Con i=-1 va in SIGSEGV)" << endl;
+    cout << endl;
+    cout << "----- TEST #4 -----" << endl;
+    cout << "RESIZE v2 - Size=5: ";
+    v2.resize(5, "Bombus");
+    for(auto el : v2)
+        cout << el << " ";
+    cout << endl;
+    cout << "----- v2 Props -----" << endl;
+    cout << "SIZE: " << v2.size() << endl;
+    cout << "CAPACITY: " << v2.capacity() << endl;
+    cout << "EMPTY?: " << v2.empty() << endl;
+    cout << endl;
+    cout << "RESIZE v2 - Size=10: ";
+    v2.resize(10, "Bombus");
+    for(auto el : v2)
+        cout << el << " ";
+    cout << endl;
+    cout << "----- v2 Props -----" << endl;
+    cout << "SIZE: " << v2.size() << endl;
+    cout << "CAPACITY: " << v2.capacity() << endl;
+    cout << "EMPTY?: " << v2.empty() << endl;
+    cout << endl;
 }
+
